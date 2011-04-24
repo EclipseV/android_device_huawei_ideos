a99b4786411c85fcca9bca3aaec925af4ceb051c
Marc Vidic <marcnvidic@ovi.com>
2011-04-24 12:43:44 +0300

    Change: cleanup

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -1,23 +1,11 @@
-# Copyright (C) 2008 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),U8150)
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),U8150)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
@@ -28,4 +16,5 @@ LOCAL_MODULE := copybit.ideos
 LOCAL_C_INCLUDES += hardware/msm7k/libgralloc
 LOCAL_CFLAGS += -DCOPYBIT_MSM7K=1
 include $(BUILD_SHARED_LIBRARY)
+
 endif # build for U8150

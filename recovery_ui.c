e7a5e07b7486175ddeb863fae3a573edba2e0428
Marc Vidic <marcnvidic@ovi.com>
2011-04-27 20:51:42 +0300

    Fix: toggling of the display
    Added missing code that returned fuctionality of KEY_POWER in recovery

--- recovery_ui.c
+++ recovery_ui.c
@@ -41,8 +41,10 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
         return 1;
+    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
     if (ui_get_showing_back_button()) {
         return 0;
+        return get_allow_toggle_display() && (key_code == KEY_POWER);
     }
     return get_allow_toggle_display() && (key_code == KEY_POWER);
 }

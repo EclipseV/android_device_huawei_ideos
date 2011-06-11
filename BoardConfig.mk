6a9320c48c7e25c7747f41d898eb67071253c7f4
tilal6991 <tilal6991@gmail.com>
2011-06-11 22:43:58 +0100

    Fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 USE_CAMERA_STUB := false
 BOARD_USE_FROYO_LIBCAMERA := true

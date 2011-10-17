699b6dd36f439188b584f7afbc545931fe3f1359
Lalit Maganti <tilal6991@gmail.com>
2011-10-17 17:45:01 +0100

    USB fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,8 +55,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := u8150
 JS_ENGINE := v8
 
 # USB mass storage
+BOARD_CUSTOM_USB_CONTROLLER := ../../device/huawei/u8150/UsbController.cpp
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun"
 BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
 
 # Sensors
